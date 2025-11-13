31d78411d0d719e0f7185cce3ba17fa283bc44f0
niragi05 <niragimasalia@gmail.com>
2025-11-13 02:06:36 -0700

    auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { caller } from "@/trpc/server";
 
-const Page = () => {
+const Page = async () => {
+  const users = await caller.getUsers();
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       <Button>Click me</Button>
+      {JSON.stringify(users)}
     </div>
   )
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,8 @@
-import { z } from 'zod';
+import prisma from '@/lib/db';
 import { baseProcedure, createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
+  getUsers: baseProcedure.query(() => {
+      return prisma.user.findMany();
     }),
 });
 // export type definition of API
