35954ec73e663fea93991adb10da5ccd996017c9
niragi05 <niragimasalia@gmail.com>
2025-12-13 23:50:40 -0700

    new trpc routes for workflows

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,10 +1,23 @@
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from 'react-error-boundary'
+import { Suspense } from "react";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
+import { WorkflowsList } from "@/features/workflows/components/workflows";
 
 const Page = async () => {
     await requireAuth();
 
+    prefetchWorkflows();
+
     return (
-        <h1>Workflows</h1>
+        <HydrateClient>
+            <ErrorBoundary fallback={<div>Error!</div>}>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <WorkflowsList />
+                </Suspense>
+            </ErrorBoundary>
+        </HydrateClient>
     )
 }
 
