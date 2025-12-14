622c68f5468a30cc1690fcec9904d06a6fbdac72
niragi05 <niragimasalia@gmail.com>
2025-12-14 00:12:59 -0700

    added common headers component

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { EntityHeader } from "@/components/entity-components";
 import { useSuspenseWorkflows } from "../hooks/use-workflows";
 
 export const WorkflowsList = () => {
@@ -10,4 +11,31 @@ export const WorkflowsList = () => {
             {JSON.stringify(workflows.data, null, 2)}
         </p>
     );
+}
+
+export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
+    return (
+        <>
+            <EntityHeader 
+                title="Workflows"
+                description="Create and manage your workflows"
+                onNew={() => {}}
+                newButtonLabel="New Workflow"
+                disabled={disabled}
+                isCreating={false}
+            />
+        </>
+    )
+}
+
+export const WorkflowsContainer = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <EntityContainer
+            header={<WorkflowsHeader />} 
+            search={<></>} 
+            pagination={<></>}
+        >
+            {children}
+        </EntityContainer>
+    )
 }
