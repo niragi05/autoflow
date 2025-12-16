4fb6372df35816772175f62a53e8df8d206c8f2f
niragi05 <niragimasalia@gmail.com>
2025-12-15 22:56:34 -0700

    added entity search for reuse

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,9 +1,27 @@
 'use client';
 
-import { EntityContainer, EntityHeader } from "@/components/entity-components";
+import { EntityContainer, EntityHeader, EntitySearch } from "@/components/entity-components";
 import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows";
 import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
 import { useRouter } from "next/navigation";
+import { useWorkflowsParams } from "../hooks/use-workflows-params";
+import { useEntitySearch } from "@/hooks/use-entity-search";
+
+export const WorkflowsSearch = () => {
+    const [params, setParams] = useWorkflowsParams();
+    const { searchValue, onSearchChange } = useEntitySearch({
+        params,
+        setParams
+    })
+    
+    return (
+        <EntitySearch 
+            value={searchValue}
+            onChange={onSearchChange}
+            placeholder="Search Workflows"
+        />
+    )
+}
 
 export const WorkflowsList = () => {
     const workflows = useSuspenseWorkflows();
@@ -52,7 +70,7 @@ export const WorkflowsContainer = ({ children }: { children: React.ReactNode }) 
     return (
         <EntityContainer
             header={<WorkflowsHeader />} 
-            search={<></>} 
+            search={<WorkflowsSearch />} 
             pagination={<></>}
         >
             {children}
