fb0d6b5ccecd9b1a29ba6f8fc19858c17fa55c27
niragi05 <niragimasalia@gmail.com>
2025-12-21 14:08:05 -0700

    added execution dialog settings and UI changes

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import type { Node, NodeProps, useReactFlow } from "@xyflow/react";
+import { type Node, type NodeProps, useReactFlow } from "@xyflow/react";
 import { GlobeIcon } from "lucide-react";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { BaseExecutionNode } from "@/features/executions/components/base-execution-node";
+import { FormType, HttpRequestDialog } from "./dialog";
 
 type HttpRequestNodeData = {
     endpoint?: string;
@@ -15,19 +16,57 @@ type HttpRequestNodeData = {
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
 
 export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const { setNodes } = useReactFlow();
+    
+    const nodeStatus = "initial";
+
+    const handleOpenSettings = () => {
+        setDialogOpen(true);
+    }
+
+    const handleSubmit = (values: FormType) => {
+        setNodes((nodes) => nodes.map((node) => {
+            if (node.id === props.id) {
+                return {
+                    ...node,
+                    data: {
+                        ...node.data,
+                        endpoint: values.endpoint,
+                        method: values.method,
+                        body: values.body,
+                    }
+                }
+            }
+            return node;
+        }))
+    }
+
     const nodeData = props.data as HttpRequestNodeData;
     const description = nodeData?.endpoint ? `${nodeData.method || "GET"}: ${nodeData.endpoint}` : "Not configured";
 
+
     return (
-        <BaseExecutionNode 
-            {...props}
-            id={props.id}
-            icon={GlobeIcon}
-            name="HTTP Request"
-            description={description}
-            onSettings={() => {}}
-            onDoubleClick={() => {}}
-        />
+        <>
+            <HttpRequestDialog 
+                open={dialogOpen} 
+                onOpenChange={setDialogOpen} 
+                onSubmit={handleSubmit}
+                defaultEndpoint={nodeData.endpoint}
+                defaultMethod={nodeData.method}
+                defaultBody={nodeData.body}
+            />
+            <BaseExecutionNode 
+                {...props}
+                id={props.id}
+                icon={GlobeIcon}
+                name="HTTP Request"
+                description={description}
+                status={nodeStatus}
+                onSettings={handleOpenSettings}
+                onDoubleClick={handleOpenSettings}
+            />
+        </>
     )
 });
 
--- src/features/triggers/components/base-trigger-node.tsx
+++ src/features/triggers/components/base-trigger-node.tsx
@@ -7,13 +7,14 @@ import { memo, type ReactNode, useCallback } from "react";
 import { BaseNode, BaseNodeContent } from "@/components/react-flow/base-node";
 import { BaseHandle } from "@/components/react-flow/base-handle";
 import { WorkflowNode } from "@/components/workflow-node";
+import { NodeStatusIndicator, type NodeStatus } from "@/components/react-flow/node-status-indicator";
 
 interface BaseTriggerNodeProps extends NodeProps {
     icon: LucideIcon | string;
     name: string;
     description?: string;
     children?: ReactNode;
-    // status?: NodeStatus;
+    status?: NodeStatus;
     onDelete?: () => void;
     onSettings?: () => void;
     onDoubleClick?: () => void;
@@ -25,6 +26,7 @@ export const BaseTriggerNode = memo(({
     name, 
     description, 
     children, 
+    status = "initial",
     onSettings, 
     onDoubleClick 
 }: BaseTriggerNodeProps) => {
@@ -49,17 +51,23 @@ export const BaseTriggerNode = memo(({
             onSettings={onSettings}
             onDelete={handleDelete}
         >
-            <BaseNode onDoubleClick={onDoubleClick} className="rounded-l-2xl relative group">
-                <BaseNodeContent>
-                    {typeof Icon === "string" ? (
-                        <Image src={Icon} alt={name} width={16} height={16} />
-                    ) : (
-                        <Icon className="size-4 text-muted-foreground" />
-                    )}
-                    {children}
-                    <BaseHandle id="source-1" type="source" position={Position.Right} />
-                </BaseNodeContent>
-            </BaseNode>
+            <NodeStatusIndicator
+                status={status}
+                variant="border"
+                className="rounded-l-2xl"
+            >
+                <BaseNode status={status} onDoubleClick={onDoubleClick} className="rounded-l-2xl relative group">
+                    <BaseNodeContent>
+                        {typeof Icon === "string" ? (
+                            <Image src={Icon} alt={name} width={16} height={16} />
+                        ) : (
+                            <Icon className="size-4 text-muted-foreground" />
+                        )}
+                        {children}
+                        <BaseHandle id="source-1" type="source" position={Position.Right} />
+                    </BaseNodeContent>
+                </BaseNode>
+            </NodeStatusIndicator>
         </WorkflowNode>
     )
 })
