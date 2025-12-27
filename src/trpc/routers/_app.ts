31676f328f18599fa93e6be34c38ab0b9b2d6f10
niragi05 <niragimasalia@gmail.com>
2025-12-26 23:13:30 -0700

    credentials endpoint and hooks

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from '../init';
 import { workflowsRouter } from '@/features/workflows/server/route';
+import { credentialsRouter } from '@/features/credentials/server/route';
 
 export const appRouter = createTRPCRouter({
 	workflows: workflowsRouter,
+	credentials: credentialsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
